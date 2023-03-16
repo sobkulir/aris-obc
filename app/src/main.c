e8257f436ebb8a05202473a2a01e8474f273b5dc
Roman Sobkuliak <r.sobkuliak@gmail.com>
2023-03-16 16:46:02 +0100

    Add skeleton

---
+++ app/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include "app_version.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+void main(void)
+{
+	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
+}
+
